25a7ae2740a146165739b2d8d5b90f803b8c5c64
the_nza <the_nza@protonmail.com>
2021-12-04 14:49:09 +0300

    Add genres' endpoints' handlers

--- backend/internal/dblab/api_routes.go
+++ backend/internal/dblab/api_routes.go
@@ -1,19 +1,26 @@
 package dblab
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/the-NZA/DB_Lab1/backend/internal/models"
 )
 
-// handles GET /api/books/:bookID
+var (
+	ErrNoIDSpecified = errors.New("You must specify ID in URL param")
+)
+
+/*
+* Books endpoints
+ */
+// handles GET /api/book/:bookID
 func (s *Server) handleBookGet() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id := chi.URLParam(r, "bookID")
 		if id == "" {
-			s.error(w, r, http.StatusInternalServerError, fmt.Errorf("You must specify ID in url param"))
+			s.error(w, r, http.StatusInternalServerError, ErrNoIDSpecified)
 			return
 		}
 
@@ -27,7 +34,7 @@ func (s *Server) handleBookGet() http.HandlerFunc {
 	}
 }
 
-// handles POST /api/books/:bookID
+// handles POST /api/book/:bookID
 func (s *Server) handleBookAdd() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		book, err := s.services.BookService().Add(models.Book{})
@@ -36,15 +43,13 @@ func (s *Server) handleBookAdd() http.HandlerFunc {
 			return
 		}
 
-		s.respond(w, r, http.StatusOK, book)
+		s.respond(w, r, http.StatusCreated, book)
 	}
 }
 
-// handles PUT /api/books/:bookID
+// handles PUT /api/book/:bookID
 func (s *Server) handleBookUpdate() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "This is update book endpoint")
-
 		book, err := s.services.BookService().Update(models.Book{})
 		if err != nil {
 			s.error(w, r, http.StatusInternalServerError, err)
@@ -55,11 +60,9 @@ func (s *Server) handleBookUpdate() http.HandlerFunc {
 	}
 }
 
-// handles DELETE /api/books/:bookID
+// handles DELETE /api/book/:bookID
 func (s *Server) handleBookDelete() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "This is delete book endpoint")
-
 		err := s.services.BookService().Delete("")
 		if err != nil {
 			s.error(w, r, http.StatusInternalServerError, err)
@@ -70,11 +73,9 @@ func (s *Server) handleBookDelete() http.HandlerFunc {
 	}
 }
 
-// handles GET /api/books/all
+// handles GET /api/book/all
 func (s *Server) handleBookGetAll() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "This is get all books endpoint")
-
 		books, err := s.services.BookService().GetAll()
 		if err != nil {
 			s.error(w, r, http.StatusInternalServerError, err)
@@ -82,5 +83,79 @@ func (s *Server) handleBookGetAll() http.HandlerFunc {
 		}
 
 		s.respond(w, r, http.StatusOK, books)
+	}
+}
+
+/*
+* Genres endpoints
+ */
+// handles GET /api/genre/:genreID
+func (s *Server) handleGenreGet() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "genreID")
+		if id == "" {
+			s.error(w, r, http.StatusInternalServerError, ErrNoIDSpecified)
+			return
+		}
+
+		genre, err := s.services.GenreService().Get(id)
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, err)
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, genre)
+	}
+}
+
+// handles POST /api/genre/:genreID
+func (s *Server) handleGenreAdd() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		genre, err := s.services.GenreService().Add(models.Genre{})
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, err)
+			return
+		}
+
+		s.respond(w, r, http.StatusCreated, genre)
+	}
+}
+
+// handles PUT /api/genre/:genreID
+func (s *Server) handleGenreUpdate() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		genre, err := s.services.GenreService().Update(models.Genre{})
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, err)
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, genre)
+	}
+}
+
+// handles DELETE /api/genre/:genreID
+func (s *Server) handleGenreDelete() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := s.services.GenreService().Delete("")
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, err)
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, "Deleted")
+	}
+}
+
+// handles GET /api/genre/all
+func (s *Server) handleGenreGetAll() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		genres, err := s.services.GenreService().GetAll()
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, err)
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, genres)
 	}
 }
