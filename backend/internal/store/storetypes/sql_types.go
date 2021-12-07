57a140a346dda4b23f2175f42963b40b00f0c75b
the_nza <the_nza@protonmail.com>
2021-12-07 16:00:44 +0300

    Add storetypes package with models for each DB types like SQL with sql.Null*** types or MongoDB bson types

---
+++ backend/internal/store/storetypes/sql_types.go
@@ -0,0 +1,79 @@
+package storetypes
+
+import (
+	"database/sql"
+
+	"github.com/the-NZA/DB_Lab1/backend/internal/models"
+)
+
+// SQLBook type add NULL values support
+type SQLBook struct {
+	ID          string         `db:"id"`
+	Title       string         `db:"title"`
+	Snippet     sql.NullString `db:"snippet"`
+	GenreID     string         `db:"genre_id"`
+	BookLangID  string         `db:"book_lang_id"`
+	PagesCnt    sql.NullInt32  `db:"pages_cnt"`
+	PublishDate sql.NullTime   `db:"pub_date"`
+	Deleted     bool           `db:"deleted"`
+}
+
+// Convert SQLBook to application's Book model
+func (sb *SQLBook) ToBookModel() models.Book {
+	book := models.Book{
+		ID:         sb.ID,
+		Title:      sb.Title,
+		GenreID:    sb.GenreID,
+		BookLangID: sb.BookLangID,
+		Deleted:    sb.Deleted,
+	}
+
+	if sb.Snippet.Valid {
+		book.Snippet = sb.Snippet.String
+	}
+
+	if sb.PagesCnt.Valid {
+		book.PagesCnt = uint(sb.PagesCnt.Int32)
+	}
+
+	if sb.PublishDate.Valid {
+		book.PublishDate = sb.PublishDate.Time
+	}
+
+	return book
+}
+
+// SQLAuthor type add NULL values support
+type SQLAuthor struct {
+	ID        string         `db:"id"`
+	Firstname string         `db:"firstname"`
+	Lastname  string         `db:"lastname"`
+	Surname   sql.NullString `db:"surname"`
+	BirthDate sql.NullTime   `db:"birth_date"`
+	Snippet   sql.NullString `db:"snippet"`
+	Deleted   bool           `db:"deleted"`
+}
+
+// Convert SQLBook to application's Author model
+func (au *SQLAuthor) ToAuthorModel() models.Author {
+	author := models.Author{
+		ID:        au.ID,
+		Firstname: au.Firstname,
+		Lastname:  au.Lastname,
+		Deleted:   au.Deleted,
+	}
+
+	if au.Snippet.Valid {
+		author.Snippet = au.Snippet.String
+	}
+
+	if au.BirthDate.Valid {
+		author.BirthDate = au.BirthDate.Time
+	}
+
+	if au.Surname.Valid {
+		author.Snippet = au.Snippet.String
+	}
+
+	return author
+}
