a8b43bf317b839c13700043aa677fc8dfdc8e699
the_nza <the_nza@protonmail.com>
2021-12-08 11:53:23 +0300

    Add some structure for frontend part with postcss plugins and basic markup and styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,52 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<header class="app-header">
+		<div class="header wrapper">
+			<h1 class="header__title">Лабораторная работа №1</h1>
+			<h3 class="header__subtitle">По дисциплине "Программирование и администрирование в среде"</h3>
+		</div>
+	</header>
+	<main class="app-main">
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<hello-world msg="Hello Vue 3 + TypeScript + Vite" />
+	</main>
+	<footer class="app-footer">
+		<div class="footer wrapper">
+			<div class="footer__credits">
+				<p>Выполнил студент группы 15.11Д-МО12/19б факультета ИМИСиЦЭ Козлов Роман</p>
+			</div>
+			<div class="footer__link">
+				<a
+					href="https://github.com/the-NZA/DB_Lab1"
+					target="_blank"
+					title="Откроется в новой вкладке"
+				>Репозиторий</a>
+			</div>
+		</div>
+	</footer>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	min-height: 100vh;
+	display: grid;
+	grid-template-rows: max-content 1fr max-content;
+	gap: var(--offset);
+}
+
+.app-header {
+	background-color: rgb(var(--leanen));
+}
+
+.app-main {
+	background-color: rgb(var(--white));
+}
+
+.app-footer {
+	background-color: rgb(var(--greyblue));
 }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import base css
+import "normalize.css"
+import "./css/variables.css"
+import "./css/core.css"
+
+// Import header css
+import "./css/header/header.css"
+
+// Import footer css
+import "./css/footer/footer.css"
+
 createApp(App).mount('#app')
