016af9702441417de960dda88126b520033709e4
the_nza <the_nza@protonmail.com>
2021-12-11 13:16:29 +0300

    Add delete and edit buttons components

---
+++ frontend/src/components/DeleteButton.vue
@@ -0,0 +1,22 @@
+<template>
+	<button @click="clicked" class="deleteBtn">Delete</button>
+</template>
+
+<script lang="ts">
+	import { ICellRendererParams } from "@ag-grid-community/all-modules";
+
+	export default {
+		name: "DeleteButton",
+		setup(props: any) {
+			const clicked = () => {
+				const params: ICellRendererParams = props.params;
+				params.context.deleteFunc(params.data.id);
+			};
+
+			return { clicked };
+		},
+	};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/EditButton.vue
@@ -0,0 +1,22 @@
+<template>
+	<button @click="clicked" class="editBtn">Edit</button>
+</template>
+
+<script lang="ts">
+	import { ICellRendererParams } from "@ag-grid-community/all-modules";
+
+	export default {
+		name: "EditButton",
+		setup(props: any) {
+			const clicked = () => {
+				const params: ICellRendererParams = props.params;
+				params.context.editFunc(params.data.id);
+			};
+
+			return { clicked };
+		},
+	};
+</script>
+
+<style>
+</style>
