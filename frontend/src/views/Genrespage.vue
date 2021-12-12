727da4388e62d4d0088f76d57795275911e03d10
the_nza <the_nza@protonmail.com>
2021-12-12 13:25:08 +0300

    Add basic genrespage structure

--- frontend/src/views/Genrespage.vue
+++ frontend/src/views/Genrespage.vue
@@ -1,16 +1,180 @@
 <template>
-	<template v-if="isGenresLoaded">
+	<!-- <template v-if="isGenresLoaded">
 		<div v-for="genre in genres" :key="genre.id">
 			{{ genre }}
 		</div>
-	</template>
+	</template>-->
+
+	<actions-buttons
+		:canEdit="singleSelected"
+		:canDelete="hasSelected"
+		@add-pressed="handleAdd"
+		@edit-pressed="handleEdit"
+		@delete-pressed="handleDelete"
+	></actions-buttons>
+
+	<ag-grid-vue
+		style="width: 100%; height: 100%;"
+		class="ag-theme-alpine"
+		:gridOptions="gridOptions"
+		:context="actionContext"
+	></ag-grid-vue>
 </template>
 
 <script lang="ts" setup>
-	import { useStore } from "../store";
-	import { storeToRefs } from "pinia";
-	const store = useStore();
-	const { isGenresLoaded, genres } = storeToRefs(store);
+import { ref, reactive, onBeforeMount, computed } from "vue";
+import { useStore } from "../store";
+import { storeToRefs } from "pinia";
+import GridButtonsVue from "../components/GridButtons.vue";
+import ActionsButtons from "../components/ActionsButtons.vue"
+import { AgGridVue } from "ag-grid-vue3";
+import { formatDate } from "../utils/date";
+import {
+	GridReadyEvent,
+	GridApi,
+	GridOptions,
+	ColumnApi,
+	RowSelectedEvent,
+	SelectionChangedEvent,
+} from "@ag-grid-community/all-modules";
+
+const store = useStore();
+const { isGenresLoaded, genres } = storeToRefs(store);
+
+// Context for action buttons
+const actionContext = ref({});
+const gridApi = ref<GridApi>();
+const columnApi = ref<ColumnApi>();
+
+// Conditional variables
+const hasSelected = ref<boolean>(false);
+const singleSelected = ref<boolean>(false);
+
+const rowData = reactive([
+	{
+		id: "1",
+		firstname: "Ivan",
+		lastname: "Ivanov",
+		surname: "Ivanovich",
+		birth_date: formatDate(new Date),
+		snippet: "This is Ivan Ivanov aka Triple I",
+	},
+	{
+		id: "2",
+		firstname: "Petr",
+		lastname: "Petrov",
+		surname: "Petrovich",
+		birth_date: new Date(1999, 8, 31).toLocaleDateString("ru", {
+			day: "2-digit",
+			month: "2-digit",
+			year: "numeric"
+		}),
+		snippet: "This is Petr Petrov aka Piper This is Petr Petrov aka Piper pThis is Petr Petrov aka Piper",
+	},
+]);
+
+const handleAdd = () => {
+	console.log("add was pressed");
+}
+
+const handleEdit = () => {
+	console.log("edit was pressed");
+}
+
+const handleDelete = () => {
+	console.log("delete was pressed");
+}
+
+const onSelectionChanged = (e: SelectionChangedEvent) => {
+	const cnt = e.api.getSelectedRows().length
+	switch (cnt) {
+		case 0:
+			hasSelected.value = false;
+			singleSelected.value = false;
+			break;
+		case 1:
+			hasSelected.value = true;
+			singleSelected.value = true;
+			break;
+		default:
+			hasSelected.value = true;
+			singleSelected.value = false;
+			break;
+
+	}
+}
+
+// Save grid and column API on grid ready event
+const onGridReady = (params: GridReadyEvent) => {
+	gridApi.value = params.api;
+	columnApi.value = params.columnApi;
+};
+
+const gridOptions = ref<GridOptions>({
+	onSelectionChanged: onSelectionChanged,
+	onGridReady: onGridReady,
+	rowSelection: "multiple",
+	suppressCellSelection: true,
+	suppressRowClickSelection: true,
+	defaultColDef: {
+		wrapText: true,
+		autoHeight: true,
+	},
+	columnDefs: [
+		{
+			field: "id",
+			hide: true,
+		},
+		{
+			field: "title",
+			checkboxSelection: true,
+			headerCheckboxSelection: true,
+			headerName: "Название",
+			flex: 2,
+			minWidth: 250,
+		},
+		{
+			field: "snippet",
+			headerName: "Описание",
+			flex: 3,
+			minWidth: 450,
+		},
+		{
+			field: "actions",
+			headerName: "Действия",
+			cellRenderer: "gridBtn",
+			// cellClass: ["test", "test2"], // Use for grid action buttons
+			width: 105,
+		}
+	],
+	rowData: rowData,
+	frameworkComponents: {
+		gridBtn: GridButtonsVue
+	},
+});
+
+// Actions funcs which passed to ag-grid
+// and called after button pressed
+const deleteFunc = (id: string) => {
+	alert(`from delete ${id}`);
+};
+const editFunc = (id: string) => {
+	alert(`from edit ${id}`);
+};
+
+// Before Mount Event handler
+onBeforeMount(() => {
+	// Setup actions context
+	actionContext.value = {
+		deleteFunc: deleteFunc,
+		editFunc: editFunc,
+	};
+});
+
+
+
+
+
 </script>
 
 <style>
