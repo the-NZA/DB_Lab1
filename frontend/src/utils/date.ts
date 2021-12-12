92855608c315db018cf56f0e09c484c5b8c2307f
the_nza <the_nza@protonmail.com>
2021-12-12 10:46:13 +0300

    add utils package with format date func

---
+++ frontend/src/utils/date.ts
@@ -0,0 +1,13 @@
+const dateOptions: Intl.DateTimeFormatOptions = {
+	day: "2-digit",
+	month: "2-digit",
+	year: "numeric",
+}
+
+export function formatDate(date: Date): string {
+	if (date) {
+		return date.toLocaleDateString("ru", dateOptions)
+	}
+
+	return "";
+}
